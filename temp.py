dab101d3354447af20146f657c79980e54af2dfe
kush45 <30504408+kush45@users.noreply.github.com>
2022-11-26 20:00:25 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,11 +1,7 @@
 # This program adds two numbers
 #!/usr/local/bin/python
 num1 = 1.5
-<<<<<<< HEAD
 num2 = 6.6
-=======
-num2 = 5.7
->>>>>>> cf6aa25c9ffdec0984ab34e9e142f557797e481a
 
 # Add two numbers
 sum = num1 + num2
