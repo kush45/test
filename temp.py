949a54ba878ce78f22f803e6a29c4e65cd56d0a1
kush45 <kushmanohar45@gmail.com>
2023-01-29 21:33:03 +0530

    add change

--- temp.py
+++ temp.py
@@ -5,7 +5,7 @@ num2 = 6.3
 
 
 # Add two numbers
-sum = num1 + num2
+sum = num1 + num
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of two number is {0} and {1} is {2}'.format(num1, num2, sum))
