84d7dbcbf856ee150ed93e9e40c56996d44726db
kush45 <30504408+kush45@users.noreply.github.com>
2023-08-13 08:45:43 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,10 +1,6 @@
 # This program adds two numbers
 #!/usr/local/bin/python
-num1 = 1.8
-
-
-# Add two numbers
-sum = num1 + num
+num
 
 # Display the sum
 print('The sum of two number is {0} and {1} is {2}'.format(num1, num2, sum))
